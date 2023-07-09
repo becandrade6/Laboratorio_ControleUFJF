8fdfbbecf7cade5a46ffe7ad11bfec2b2dcd1ae5
Bernardo Andrade <bernardo.andrade@engenharia.ufjf.br>
2023-07-09 16:06:06 -0300

    Adicionando imagens dos testes

--- Malha_Fechada/Python/grafico_controlador.py
+++ Malha_Fechada/Python/grafico_controlador.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import numpy as np
+import plotly.graph_objects as go
 
 
 def divide(x):
@@ -12,25 +13,36 @@ def porcento(x):
 
 
 headers = ["Temperatura (°C)", "Tempo (s)", "Saida para relé (% da rede)", "SetPoint"]
-df = pd.read_csv("data03_07_P35_I0-005_T60.csv", names=headers)
+df = pd.read_csv("data05_06_CProporcional30.csv", names=headers)
 
-df["SetPoint"] = 60.0
+df["SetPoint"] = 80.0
 df["Tempo (s)"] = df["Tempo (s)"].map(divide)
 
 df["Saida para relé (% da rede)"] = df["Saida para relé (% da rede)"].map(porcento)
 
-indices = np.where(df["Temperatura (°C)"].to_numpy() >= 60 * 0.98)
+indices = np.where(df["Temperatura (°C)"].to_numpy() >= 80 * 0.98)
 
 fig = px.line(
     df,
     y=["Temperatura (°C)", "Saida para relé (% da rede)", "SetPoint"],
     x="Tempo (s)",
-    title="Gráfico Controlador PI Kp = 35/ Ki = 0,005 - Setpoint 60°C",
+    title="Gráfico Controlador P Kp = 30 - Setpoint 80°C",
 )
 
-fig.add_vline(df["Tempo (s)"].to_numpy()[indices[0][0]])
+# fig.add_trace(
+# go.Scatter(
+#  x=[df["Tempo (s)"][indices[0][0]], df["Tempo (s)"][indices[0][0]]],
+#   y=[0, df["Saida para relé (% da rede)"].max()],
+#    mode="lines",
+#     line=dict(color="black", width=2, dash="dash"),
+#      name="Tempo acomodação (2%)",
+#   )
+# )
 
 fig.update_yaxes(title_text="Temperatura (°C)")
+
+fig.update_layout(font_size=20, legend_font_size=15)
+
 fig.show()
 
 
